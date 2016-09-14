f74712cd997e922d979468491726e0108f338bde
themike123 <themike123nova@gmail.com>
2016-09-14 08:00:24 -0500

    se quito el floter donde aun estaba

--- application/views/questions/index.php
+++ application/views/questions/index.php
@@ -256,7 +256,7 @@
 
 	        <footer class="footer">
 	            <div class="container-fluid">
-	                <nav class="pull-left">
+	                <!--nav class="pull-left">
 	                    <ul>
 
 	                        <li>
@@ -278,7 +278,7 @@
 	                </nav>
 	                <div class="copyright pull-right">
 	                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>
-	                </div>
+	                </div-->
 	            </div>
 	        </footer>
 
