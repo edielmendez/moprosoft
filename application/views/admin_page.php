8b71b10cb8ef5299b8d0a2395a761504d6bd5b41
edielmendez <mendezjunior2015@gmail.com>
2016-09-20 16:40:13 -0500

    CRUD equipos y CRUD estudiantes y se quito el footer de estas interfaces

--- application/views/admin_page.php
+++ application/views/admin_page.php
@@ -160,7 +160,7 @@
 
 
 	        <footer class="footer">
-	            <div class="container-fluid">
+	            <!--<div class="container-fluid">
 	                <nav class="pull-left">
 	                    <ul>
 
@@ -184,7 +184,7 @@
 	                <div class="copyright pull-right">
 	                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>
 	                </div>
-	            </div>
+	            </div>-->
 	        </footer>
 
 	    </div>
